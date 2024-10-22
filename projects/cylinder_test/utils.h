467a5b39eaa0a0be265e633b862ebff81e0cdcb4
Luca Wolfart <wolfartl@ethz.ch>
2024-10-22 10:21:32 +0200

    add function to compute H field

--- projects/cylinder_test/utils.h
+++ projects/cylinder_test/utils.h
@@ -16,25 +16,42 @@ class MeshFunctionCurl2DFE{
     public:
         using Scalar = decltype(SCALAR_FE(0) * SCALAR_COEFF(0));
         MeshFunctionCurl2DFE(lf::fe::MeshFunctionGradFE<SCALAR_FE, SCALAR_COEFF > gradients) : mf_gradients_(gradients){}
+        MeshFunctionCurl2DFE(const MeshFunctionCurl2DFE&) = default; 
         std::vector<Eigen::Matrix< Scalar, Eigen::Dynamic, 1>> operator() (const lf::mesh::Entity &e, const Eigen::MatrixXd &local) const {
             std::vector<Eigen::Matrix<Scalar, Eigen::Dynamic, 1>> result(local.cols());
             const std::vector<Eigen::Matrix<Scalar, Eigen::Dynamic, 1>> gradients = mf_gradients_(e, local);
             for (Eigen::Index i = 0; i < local.cols(); ++i){
                 Eigen::Vector2d temp;
-                //temp << gradients[i][1] , -gradients[i][0];
                 temp[0] = gradients[i][1];
                 temp[1] = -gradients[i][0];
                 result[i] = temp;
-                // if (i == 0) {std::cout << "gradients shape" << gradients[i].size() << std::endl;}
-
-                // result[i][0] = gradients[i][1];
-                // result[i][1] = -gradients[i][0];
             }
             return result; 
         }
     private:
-        lf::fe::MeshFunctionGradFE<SCALAR_FE, SCALAR_COEFF > mf_gradients_;
+        const lf::fe::MeshFunctionGradFE<SCALAR_FE, SCALAR_COEFF > mf_gradients_;
 };
+
+template<class SCALAR_FE, class SCALAR_COEFF> 
+class MeshFunctionH{
+    public:
+        using Scalar = decltype(SCALAR_FE(0) * SCALAR_COEFF(0));
+        MeshFunctionH(const MeshFunctionCurl2DFE<SCALAR_FE, SCALAR_COEFF> curl, lf::mesh::utils::CodimMeshDataSet<SCALAR_COEFF> permeability) : mf_curl_(curl), permeability_(permeability) {}
+        std::vector<Eigen::Matrix< Scalar, Eigen::Dynamic, 1>> operator() (const lf::mesh::Entity &e, const Eigen::MatrixXd &local) const {
+            auto result = mf_curl_(e, local);
+            double permeability = permeability_(e);
+            // Divide each matrix in the vector by the scalar
+            for (auto& matrix : result) {
+                matrix /= permeability;
+            }
+            return result;
+        }
+    private:
+        const MeshFunctionCurl2DFE<SCALAR_FE, SCALAR_COEFF > mf_curl_;
+        lf::mesh::utils::CodimMeshDataSet<double> permeability_;
+};
+
+
 } //namespace utils
 
 #endif // UTILS_H
