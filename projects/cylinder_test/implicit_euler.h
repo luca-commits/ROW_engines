62bd9df0da38befee0cdea1673b18d67f50ff8e8
Luca Wolfart <wolfartl@ethz.ch>
2024-10-22 10:16:17 +0200

    use cholesky instead of qr

--- projects/cylinder_test/implicit_euler.h
+++ projects/cylinder_test/implicit_euler.h
@@ -6,26 +6,33 @@
 #include <iostream> 
 
 #include <lf/fe/fe.h>
+#include "BICG_stab.hpp"
 
-Eigen::VectorXd implicit_euler_step(Eigen::SparseMatrix<double> A, Eigen::SparseMatrix<double> M, double timestep, Eigen::VectorXd current_step, Eigen::VectorXd load_vector){
-    Eigen::SparseMatrix<double> lhs = timestep * A + M ;
+Eigen::VectorXd implicit_euler_step(const Eigen::SparseMatrix<double>& A, const Eigen::SparseMatrix<double>& M, double timestep, const Eigen::VectorXd & current_step, const Eigen::VectorXd & load_vector){
+    // Eigen::SparseMatrix<double> Id(load_vector.size(), load_vector.size());
+    // Id.setZero(); 
+    // std::cout << "M " << std::endl << M << std::endl;
+    Eigen::SparseMatrix<double> lhs = timestep * A + M;
     Eigen::VectorXd rhs = M * current_step + timestep * load_vector; 
-    // std::cout << "solving qr" << std::endl;
-    Eigen::SparseQR<Eigen::SparseMatrix<double>, Eigen::COLAMDOrdering<int>> qr;
-    qr.compute(lhs);
-    Eigen::VectorXd sol_vec = qr.solve(rhs);
-    if(sol_vec.norm() != 0){
-        double rel_residual = (lhs * sol_vec - rhs).norm() / sol_vec.norm();
-        std::cout << "relative residual : " << rel_residual << std::endl; 
-        // LF_ASSERT_MSG(rel_residual < 10e-7,
-        //             " solver failed, residual is greater than 10e-7");
+    lf::assemble::dim_t N_dofs = load_vector.size();
+    Eigen::VectorXd next_timestep(N_dofs);
+    Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> solver;
+
+    solver.compute(lhs);
+    next_timestep = solver.solve(rhs);
+
+    // std::cout << "rhs " << std::endl << rhs << std::endl;
+
+    double rel_res = 0.0;
+    if (rhs.norm() != 0) {
+        rel_res = (lhs * next_timestep - rhs).norm() / rhs.norm();
+        if (rel_res > 1e-7) {
+            LF_ASSERT_MSG(rel_res < 1e-7, "Solver failed, residual is greater than 1e-7");
+        }
+    } else {
+        std::cout <<"Not normalized residuum " <<  lhs * next_timestep - rhs << std::endl;
     }
-    // std::cout << "finished solving qr" << std::endl ;
-    std::cout << "backwards difference: " << (sol_vec - current_step).norm() << std::endl;
 
-
-    return sol_vec;
+    return next_timestep;
 }
-
-
 #endif //IMPLICIT_EULER_H
