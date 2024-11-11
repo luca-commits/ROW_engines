5c17e3dcd14e22ddffc45ffab1a10579870d2c69
Luca Wolfart <wolfartl@ethz.ch>
2024-11-11 16:01:04 +0100

    plotting tool

--- projects/cylinder_test/Analysis/plot_field_along_radius.py
+++ projects/cylinder_test/Analysis/plot_field_along_radius.py
@@ -7,7 +7,7 @@ mu = 0.999994 * 4 * np.pi * 1e-7
 
 # Load the data from the CSV file
 # dataframe_004_spherical = pd.read_csv("transient/conductivity_10000.csv")
-dataframe_004_spherical = pd.read_csv("transient/conductivity_10000.csv")
+dataframe_004_spherical = pd.read_csv("transient/test1.csv")
 
 # Filter the dataframe to include only rows where the arc_length is less than 10
 dataframe_004_spherical = dataframe_004_spherical[dataframe_004_spherical['arc_length'] < 5]
@@ -29,9 +29,12 @@ plt.Figure()
 B_magnitude_004_spherical = (dataframe_004_spherical['B:0'].apply(np.square) + dataframe_004_spherical['B:1'].apply(np.square)).apply(np.sqrt)
 B_magnitude_004_spherical = B_magnitude_004_spherical
 
+
 plt.plot(arc_length, analytic, label="analytic solution")
-plt.plot(dataframe_004_spherical['arc_length'], B_magnitude_004_spherical, label="mesh size 0.04, spherical air box")
+plt.plot(dataframe_004_spherical['arc_length'], B_magnitude_004_spherical, label="numeric solution")
 plt.legend()
+plt.xlabel("radius along cylinder")
+plt.ylabel("B field")
 plt.savefig("B_field_for_various_meshes.png")
 # plt.plot(dataframe['arc_length'], analytic_sq)
 plt.show()
