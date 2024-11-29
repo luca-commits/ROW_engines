192e117ddcc4323e2e4349c8470564f88c731026
Luca Wolfart <wolfartl@ethz.ch>
2024-11-29 10:04:38 +0100

    newton step implementation (not working)

--- projects/cylinder_test/implicit_euler.h
+++ projects/cylinder_test/implicit_euler.h
@@ -8,7 +8,7 @@
 #include <lf/fe/fe.h>
 #include "BICG_stab.hpp"
 
-Eigen::VectorXd implicit_euler_step(const Eigen::SparseMatrix<double>& A, const Eigen::SparseMatrix<double>& M, double timestep, const Eigen::VectorXd & current_step, const Eigen::VectorXd & load_vector){
+Eigen::VectorXd implicit_euler_step(const Eigen::SparseMatrix<double>& A, const Eigen::SparseMatrix<double>& M, double timestep, const Eigen::VectorXd & current_step,  const Eigen::VectorXd & load_vector){
     Eigen::SparseMatrix<double> lhs = timestep * A + M;
     std::cout << "lhs.norm() " << lhs.norm() << std::endl;
     Eigen::VectorXd rhs = M * current_step + timestep * load_vector; 
@@ -20,7 +20,6 @@ Eigen::VectorXd implicit_euler_step(const Eigen::SparseMatrix<double>& A, const 
     solver.compute(lhs);
     next_timestep = solver.solve(rhs);
     std::cout << "next_timestep.norm() " << next_timestep.norm() << std::endl;
-
 
     double rel_res = 0.0;
     if (rhs.norm() != 0) {
@@ -34,5 +33,38 @@ Eigen::VectorXd implicit_euler_step(const Eigen::SparseMatrix<double>& A, const 
 
     return next_timestep;
 }
+
+Eigen::VectorXd newton_step(const Eigen::SparseMatrix<double>& N, 
+                            const Eigen::SparseMatrix<double>& A, 
+                            const Eigen::SparseMatrix<double>& M, 
+                            double timestep, 
+                            const Eigen::VectorXd & previous_newton_step, 
+                            const Eigen::VectorXd & previous_time_step,  
+                            const Eigen::VectorXd & rho, 
+                            const Eigen::VectorXd & phi){
+
+    Eigen::SparseMatrix<double> lhs = A * timestep + M + N * timestep ;
+    Eigen::VectorXd rhs = - phi * timestep - M * previous_time_step + rho * timestep + M * previous_newton_step;
+    lf::assemble::dim_t N_dofs = phi.size();
+    Eigen::VectorXd next_timestep(N_dofs);
+    Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> solver;
+
+    solver.compute(lhs);
+    next_timestep = solver.solve(rhs);
+
+    double rel_res = 0.0;
+    if (rhs.norm() != 0) { 
+        rel_res = (lhs * next_timestep - rhs).norm() / rhs.norm();
+        if (rel_res > 1e-7) {
+            LF_ASSERT_MSG(rel_res < 1e-7, "Solver failed, residual is greater than 1e-7");
+        }
+    } else {
+        std::cout <<"Solution vector is 0" << std::endl;
+    }
+
+    return next_timestep;
+}
+
+
 
 #endif //IMPLICIT_EULER_H
