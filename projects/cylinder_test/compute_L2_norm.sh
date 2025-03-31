c8f103dcd8fda6e5ab52a9bcde85480c55a1cd34
Luca Wolfart <wolfartl@ethz.ch>
2025-03-31 22:29:25 +0200

    scripts to evaluate the L2 error norm

--- projects/cylinder_test/compute_L2_norm.sh
+++ projects/cylinder_test/compute_L2_norm.sh
@@ -1,3 +1,22 @@
-cp ../../build/projects/cylinder_test/benchmark_file_newton_methods_box_irregular_ring.csv .
-cp ../../build/projects/cylinder_test/benchmark_file_rosenbrock-wanner_box_irregular_ring.csv .
-python3 compute_L2_norm.py benchmark_file_rosenbrock-wanner_box_irregular_ring.csv benchmark_file_newton_methods_box_irregular_ring.csv 
+cp ../../build/projects/cylinder_test/benchmark_file_rosenbrock-wanner_transformator_magnetic_field.csv .
+cp ../../build/projects/cylinder_test/benchmark_file_rosenbrock-wanner_transformator_current.csv .
+# cp ../../build/projects/cylinder_test/benchmark_file_newton_methods_small_timestep_transformator_B_field.csv . 
+# cp ../../build/projects/cylinder_test/benchmark_file_newton_methods_small_timestep_transformator_B_power.csv . 
+# cp ../../build/projects/cylinder_test/benchmark_file_newton_methods_small_timestep_transformator_current.csv . 
+cp ../../build/projects/cylinder_test/benchmark_file_newton_methods_transformator_B_field.csv . 
+cp ../../build/projects/cylinder_test/benchmark_file_newton_methods_transformator_current.csv . 
+cp ../../build/projects/cylinder_test/benchmark_file_newton_methods_transformator_B_power.csv . 
+cp ../../build/projects/cylinder_test/benchmark_file_newton_methods_transformator_current.csv . 
+cp ../../build/projects/cylinder_test/benchmark_file_rosenbrock-wanner_transformator_magnetic_power.csv . 
+echo "\nL2 norm B field energy, BDF2 vs benchmark"
+python3 compute_L2_norm.py benchmark_file_rosenbrock-wanner_transformator_magnetic_field.csv benchmark_file_newton_methods_small_timestep_transformator_B_field.csv
+echo "\nL2 norm B field energy, ROW vs benchmark"
+python3 compute_L2_norm.py benchmark_file_newton_methods_transformator_B_field.csv benchmark_file_newton_methods_small_timestep_transformator_B_field.csv
+echo "\nL2 norm current, ROW vs Benchmark"
+python3 compute_L2_norm.py benchmark_file_rosenbrock-wanner_transformator_current.csv benchmark_file_newton_methods_small_timestep_transformator_current.csv
+echo "\nL2 norm current, BDF2 vs Benchmark"
+python3 compute_L2_norm.py  benchmark_file_newton_methods_transformator_current.csv benchmark_file_newton_methods_small_timestep_transformator_current.csv
+echo "\nL2 norm total power loss, ROW vs Benchmark"
+python3 compute_L2_norm.py benchmark_file_rosenbrock-wanner_transformator_magnetic_power.csv benchmark_file_newton_methods_small_timestep_transformator_B_power.csv
+echo "\nL2 norm total power loss, BDF2 vs Benchmark"
+python3 compute_L2_norm.py  benchmark_file_newton_methods_transformator_B_power.csv benchmark_file_newton_methods_small_timestep_transformator_B_power.csv
