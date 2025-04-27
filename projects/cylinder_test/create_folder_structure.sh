d59ba5ef26939cda1ea1602e8530ca7fd9fbc758
Luca Wolfart <wolfartl@ethz.ch>
2025-04-27 13:52:12 +0200

    modify folder structure creation file

--- projects/cylinder_test/create_folder_structure.sh
+++ projects/cylinder_test/create_folder_structure.sh
@@ -3,22 +3,19 @@
 # Set the base directory (current directory is /projects/cylinder_test/)
 base_dir=$(pwd)
 
-# Define the parent projects directory (../../build_test/projects/cylinder_test)
-projects_dir="$base_dir/../../build/projects/cylinder_test"
+# Define the parent projects directory (../../build/projects/cylinder_test)
+projects_dir="$base_dir/../../build/projects/cylinder_test/vtk_files/time_dependent/"
 
-# Define the directories to create (based on the structure in the screenshot)
+# Define the directories to create (based on the structure from previous answer)
 directories=(
-    "$projects_dir/time_dependent"
     "$projects_dir/non-linear"
     "$projects_dir/non-linear/box_irregular_ring"
     "$projects_dir/non-linear/box_irregular_ring/bdf1"
     "$projects_dir/non-linear/box_irregular_ring/bdf2"
-    "$projects_dir/transformator"
-    "$projects_dir/transformator/bdf1"
-    "$projects_dir/transformator/bdf2"
-    "$projects_dir/transformator/presentation"
-    "$projects_dir/transformator/row_dynamic"
-    "$projects_dir/transformator/row_static"
+    "$projects_dir/non-linear/transformator/bdf1"
+    "$projects_dir/non-linear/transformator/bdf2"
+    "$projects_dir/row_dynamic"
+    "$projects_dir/row_static"
 )
 
 # Create the directories if they don't exist
@@ -32,4 +29,3 @@ for dir in "${directories[@]}"; do
 done
 
 echo "Folder structure replicated successfully under $projects_dir."
-
